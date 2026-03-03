a925f2fecf78697d404b05e755fb01da341f87dd
nishioko <kosuke.nishio@jp.ricoh.com>
2026-03-03 11:39:09 +0900

    fix: _invalidateAndReload の余分な閉じ括弧を削除しコンパイルエラーを修正

--- lib/screens/today_watering_screen.dart
+++ lib/screens/today_watering_screen.dart
@@ -141,7 +141,6 @@ class _TodayWateringScreenState extends State<TodayWateringScreen> {
     });
     await _loadLogsForDate(date);
   }
-  }
 
   /// 指定日に表示すべき植物リストを決定する
   List<Plant> _getPlantsForDate(
